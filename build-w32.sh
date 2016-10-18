640af05767dd77fee094df65007e0f7fd2ec1814
Andrew Cain <acain@swin.edu.au>
2016-10-17 20:42:19 -0700

    NEW: Add build for w32

---
+++ build-w32.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+gcc -shared -Wall -Wextra -Wshadow -Wformat-security -Winit-self -Wmissing-prototypes \
+  -O2 -DWIN32 -DNDEBUG \
+  -Iinclude \
+  src/civetweb.cpp \
+  -lws2_32 -mwindows -lmsvcr100 \
+  -static-libstdc++ -static-libgcc \
+  -Wl,-Bstatic -lstdc++ -lpthread \
+  -o libcivetweb.dll
