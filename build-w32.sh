0c5657413f09e454d9ad12fa8d8624d262d59cae
Andrew Cain <acain@swin.edu.au>
2016-10-17 20:50:03 -0700

    FIX: Update scripts to build c file

--- build-w32.sh
+++ build-w32.sh
@@ -3,7 +3,7 @@
 gcc -shared -Wall -Wextra -Wshadow -Wformat-security -Winit-self -Wmissing-prototypes \
   -O2 -DWIN32 -DNDEBUG \
   -Iinclude \
-  src/civetweb.cpp \
+  src/civetweb.c \
   -lws2_32 -mwindows -lmsvcr100 \
   -static-libstdc++ -static-libgcc \
   -Wl,-Bstatic -lstdc++ -lpthread \
--- build-w64.sh
+++ build-w64.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-g++ -shared src/civetweb.cpp -I ./include/ -lws2_32 -mwindows -static-libstdc++ -static-libgcc -Wl,-Bstatic -lstdc++ -lpthread -o libcivetweb.dll
+gcc -m64 -shared -Wall -Wextra -Wshadow -Wformat-security -Winit-self -Wmissing-prototypes \
+  -O2 -DWIN32 -DNDEBUG \
+  -Iinclude \
+  src/civetweb.c \
+  -lws2_32 -mwindows -lmsvcr100 \
+  -static-libstdc++ -static-libgcc \
+  -Wl,-Bstatic -lstdc++ -lpthread \
+  -o libcivetweb.dll
